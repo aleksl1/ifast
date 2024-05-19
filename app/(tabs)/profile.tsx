d430143cde19c683a9704c5d4c6da7c05fd5912a
aleksl1 <aleksander.slawek1@gmail.com>
2024-05-19 21:17:09 +0200

    feat: add profile page boilerplate

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,73 @@
+import { keys } from "@/store/storageKeys";
 import { StyleSheet, View } from "react-native";
+import { useMMKVString } from "react-native-mmkv";
+import { Avatar, TextInput, Text, Surface } from "react-native-paper";
 
 export default function ProfileScreen() {
-  return <View style={styles.container}></View>;
+  const [name, setName] = useMMKVString(keys.name);
+
+  //todo: waga początkowa, waga aktualna, pierwszy ukończony post - data, ukończonych postów w miesiącu, całkowity czas postów, najwięcej razy ukończono post X,
+
+  return (
+    <View style={styles.container}>
+      <Avatar.Image size={100} source={require("../../assets/avatar.jpg")} />
+      <TextInput
+        label="imie"
+        mode="outlined"
+        value={name}
+        onChangeText={(value) => setName(value)}
+      />
+      <View style={styles.surfaceContainer}>
+        <Surface style={styles.surface} elevation={4}>
+          <Text variant="titleLarge">0</Text>
+          <Text variant="titleSmall" style={{ textAlign: "center" }}>
+            ukończonych postów
+          </Text>
+        </Surface>
+        <Surface style={styles.surface} elevation={4}>
+          <Text>Surface</Text>
+        </Surface>
+        <Surface style={styles.surface} elevation={4}>
+          <Text variant="titleLarge">0</Text>
+          <Text variant="titleSmall" style={{ textAlign: "center" }}>
+            ukończonych postów
+          </Text>
+        </Surface>
+        <Surface style={styles.surface} elevation={4}>
+          <Text variant="titleLarge">0</Text>
+          <Text variant="titleSmall" style={{ textAlign: "center" }}>
+            ukończonych postów
+          </Text>
+        </Surface>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 16,
+    margin: 16,
+    marginTop: 24,
     alignItems: "center",
+  },
+  surfaceContainer: {
+    // flex: 1,
+    alignSelf: "stretch",
+    flexDirection: "row",
+    flexWrap: "wrap",
     justifyContent: "center",
+    gap: 32,
+    margin: 16,
+  },
+  surface: {
+    padding: 8,
+    height: 120,
+    width: 120,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderRadius: 8,
+    backgroundColor: "white",
   },
 });
--- store/storageKeys.ts
+++ store/storageKeys.ts
@@ -2,6 +2,7 @@ export const keys = {
   selectedFast: "selectedFast",
   fastList: "fastList",
   savedTime: "savedTime",
+  name: "name",
 };
 
 export const getFastKey = (i: number) => `${keys.fastList}-${i}`;
